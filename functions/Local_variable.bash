791c0fa69c990e0bb7b49dfdc74c65a1c1f44ef7
Dennis Tei-Muno <munodennis@gmail.com>
2024-11-15 02:04:09 -0600

    new adds

---
+++ functions/Local_variable.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#can only be accesss within the function
+#create using the "local" keyword
+# - local LOCAL_VAR=1
+#Only functions can have local variables
+#Best practice to keep variables local in functions
+
---
+++ functions/exercise.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+function file_count() {
+    local NUMBER_OF_FILES=$(ls | wc -l)
+    echo "$NUMBER_OF_FILES"
+}
+
+file_count
