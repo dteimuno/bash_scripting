71d33451bf2d16203bf0a0806e6766749dd1565f
Dennis Tei-Muno <munodennis@gmail.com>
2024-11-15 02:30:19 -0600

    Create shell_script_order_and_checklist.bash

---
+++ shell_script_checklist_and_template/shell_script_order_and_checklist.bash
@@ -0,0 +1,7 @@
+# 1.shebang
+#2. comments/file header
+#3. global variables
+#4. functions: use local variables
+#5. main script contents
+#6. exit with an exit status
+#6a exit <STATUS> at various exit points.
